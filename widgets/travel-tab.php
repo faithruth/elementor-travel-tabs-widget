b54f0f230a5afa4bbdb04e5eebaae74d803d9fce
faithruth <faithruth27@gmail.com>
2025-01-06 16:14:50 +0300

    removed style tab

--- widgets/travel-tab.php
+++ widgets/travel-tab.php
@@ -103,32 +103,6 @@ class Elementor_Travel_Tabs_Widget extends \Elementor\Widget_Base {
 
 		// Content Tab End
 
-
-		// Style Tab Start
-
-		$this->start_controls_section(
-			'section_title_style',
-			[
-				'label' => esc_html__( 'Title', 'elementor-addon' ),
-				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		$this->add_control(
-			'title_color',
-			[
-				'label' => esc_html__( 'Text Color', 'elementor-addon' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .hello-world' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->end_controls_section();
-
-		// Style Tab End
-
 	}
 
     private function tab_section( $tab ) {
