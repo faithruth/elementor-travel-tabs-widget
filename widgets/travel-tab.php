8d2d4a232d943aff7496eecc7f8ad9e1292855cc
faithruth <faithruth27@gmail.com>
2025-01-13 15:58:31 +0300

    Clean ups

--- widgets/travel-tab.php
+++ widgets/travel-tab.php
@@ -554,7 +554,7 @@ class Elementor_Travel_Tabs_Widget extends \Elementor\Widget_Base {
             }
             ?>
             </ul>
-            <button class="button button--reversed next-tab" data-next="1" data-show=" . $next_tab . "><?php echo __('View itinerary details', 'elementor-travel-tabs-widget'); ?></button>
+            <button class="button button--reversed next-tab travel-tab__button" data-next="1" data-show=" . $next_tab . "><?php echo __('View itinerary details', 'elementor-travel-tabs-widget'); ?></button>
             </div>
             <div>
                 <figure class="travel-tab__activity_map"><img src="<?php echo $activity_map['url']; ?>" alt="<?php echo $activity_map['alt']; ?>"></figure>
